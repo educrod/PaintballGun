f10e5f8bdd0e21fb6b3fdb0f1b07fa6290d0ed82
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-05-12 00:42:06 -0300

    Add project files.

---
+++ PaintballGun/PaintballGun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintballGun
+{
+    class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+        private int balls = 0; 
+        private int ballsLoaded = 0;
+        public int GetBallsLoaded() { return ballsLoaded; }
+        public bool IsEmpty() { return ballsLoaded == 0; }
+        
+        public int Balls 
+        {
+            get { return balls; }
+            set            
+            {
+                if (value > 0)
+                    balls = value; Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+                ballsLoaded = MAGAZINE_SIZE;
+            else
+                ballsLoaded = balls;
+        }
+        
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false; 
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
